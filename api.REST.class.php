ece6d3f3b45168ac153818e360da57870e266736
Sergio Jiménez González <sjimgon430@g.educaand.es>
2025-01-17 11:42:40 +0100

    Aplicación REST

--- api.REST.class.php
+++ api.REST.class.php
@@ -16,12 +16,12 @@ class apiRest{
         $method = $_SERVER['REQUEST_METHOD'];//Tenemos que tener en cuenta que método se está utilizando
         $response = null;//Aquí guardaremos la respuesta que devolveremos al cliente segun el protocolo(JSON)
 
-        //Función   Verbo    URL
-        //INDEX     GET      arrayUri[0] /                              //Obtener todos los registros
-        //SHOW      GET      arrayUri[0] / id(arrayUri[1])              //Obtener un registro por id
-        //STORE     POST     arrayUri[0] / store                        //Formulario para crear un registro
-        //UPDATE    PUT      arrayUri[0] / update / id(arrayUri[2])     //Actualizar un registro con los datos procedentes de un formulario -> No es posible mediante un formulario, utilizaremos POST
-        //DELETE    DELETE   arrayUri[0] / destroy / id(arrayUri[2])    //Eliminar un registro -> No es posible mediante un formulario, utilizaremos POST
+        //Función   Verbo           URL
+        //INDEX     GET             arrayUri[0] /                              //Obtener todos los registros
+        //SHOW      GET             arrayUri[0] / id(arrayUri[1])              //Obtener un registro por id
+        //STORE     POST            arrayUri[0] / store                        //Formulario para crear un registro
+        //UPDATE    PUT(POST)       arrayUri[0] / update / id(arrayUri[2])     //Actualizar un registro con los datos procedentes de un formulario -> No es posible mediante un formulario, utilizaremos POST
+        //DELETE    DELETE(post)    arrayUri[0] / destroy / id(arrayUri[2])    //Eliminar un registro -> No es posible mediante un formulario, utilizaremos POST
         
         switch ($method) {
             case 'GET':
